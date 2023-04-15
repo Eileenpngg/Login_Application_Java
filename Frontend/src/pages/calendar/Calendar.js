d86a5281b6b1de5bcf2c6f49a6a877eef9de235b
Eileenpngg <eileenpng1843@gmail.com>
2023-04-15 23:47:07 +0800

    'update calendar'

--- Frontend/src/pages/calendar/Calendar.js
+++ Frontend/src/pages/calendar/Calendar.js
@@ -9,62 +9,74 @@ return(
         <h1 class={`${styles.h1}`}>Calendar</h1>
         <div class={`${styles.row}`}>
 
-            <Card class={`${styles.card}`}>
+            <Card class={`${styles.card}`} style={{borderBottomColor: "#F08080", borderBottomStyle: "solid", borderWidth:"30px"}}>
                 <h3 style={{margin: "0px"}}>January</h3>
                 <p style={{margin: "2px"}}>Jan 1: New year</p>
             </Card>
 
-            <Card class={`${styles.card}`}>
+            <Card class={`${styles.card}`} style={{borderBottomColor: "#FFDEB4", borderBottomStyle: "solid" ,borderWidth:"30px"}}>
                 <h3 style={{margin: "0px"}}>February</h3>
                 <p style={{margin: "2px"}}>Feb 1 to 3: CNY</p>
             </Card>
 
-            <Card class={`${styles.card}`}>March</Card>
+            <Card class={`${styles.card}`} style={{borderBottomColor: "#DFA67B", borderBottomStyle: "solid" ,borderWidth:"30px"}}>
+                <h3 style={{margin: "0px"}}>March</h3>
+            </Card>
         </div>
 
         <div class={`${styles.row}`}>
-            <Card class={`${styles.card}`}>
+            <Card class={`${styles.card}`} style={{borderBottomColor: "#E5FDD1", borderBottomStyle: "solid" ,borderWidth:"30px"}}>
                 <h3 style={{margin: "0px"}}>April</h3>
                 <p style={{margin: "2px"}}>15 April: Good Friday</p>
             </Card>
 
-            <Card class={`${styles.card}`}>
+            <Card class={`${styles.card}`} style={{borderBottomColor: "#FCC2FC", borderBottomStyle: "solid" ,borderWidth:"30px"}}>
                 <h3 style={{margin: "0px"}}>May</h3>
-                <p style={{margin: "2px"}}>1 May: Labour Day*</p> //2nd May
+                <p style={{margin: "2px"}}>1 May: Labour Day*</p>
                 <p style={{margin: "2px"}}>3 May: Hari Raya Puasa</p>
             </Card>
 
-            <Card class={`${styles.card}`}>
+            <Card class={`${styles.card}`} style={{borderBottomColor: "#EAC7C7", borderBottomStyle: "solid" ,borderWidth:"30px"}}>
                 <h3 style={{margin: "0px"}}>June</h3>
             </Card>
         </div>
 
         <div class={`${styles.row}`}>
-            <Card class={`${styles.card}`}>
+            <Card class={`${styles.card}`} style={{borderBottomColor: "#A0C3D2", borderBottomStyle: "solid" ,borderWidth:"30px"}}>
                 <h3 style={{margin: "0px"}}>July</h3>
-                <p style={{margin: "2px"}}>10 July: Hari Raya Haji*</p> //11 july
+                <p style={{margin: "2px"}}>10 July: Hari Raya Haji*</p>
             </Card>
 
-            <Card class={`${styles.card}`}>
+            <Card class={`${styles.card}`} style={{borderBottomColor: "#FD8A8A", borderBottomStyle: "solid" ,borderWidth:"30px"}}>
                 <h3 style={{margin: "0px"}}>August</h3>
                 <p style={{margin: "2px"}}>9 August: National Day</p>
             </Card>
 
-            <Card class={`${styles.card}`}>September</Card>
+            <Card class={`${styles.card}`} style={{borderBottomColor: "#9EA1D4", borderBottomStyle: "solid" ,borderWidth:"30px"}}>
+                <h3 style={{margin: "0px"}}>September
+                </h3>
+            </Card>
         </div>
 
         <div class={`${styles.row}`}>
-            <Card class={`${styles.card}`}>
+            <Card class={`${styles.card}`} style={{borderBottomColor: "#F08080", borderBottomStyle: "solid" ,borderWidth:"30px"}}>
                 <h3 style={{margin: "0px"}}>October</h3>
                 <p style={{margin: "2px"}}>24 October: Deepavali</p>
             </Card>
-            <Card class={`${styles.card}`}>November</Card>
-            <Card class={`${styles.card}`}>
+
+            <Card class={`${styles.card}`} style={{borderBottomColor: "#FFDEB4", borderBottomStyle: "solid" ,borderWidth:"30px"}}>
+                <h3 style={{margin: "0px"}}>November</h3>
+            </Card>
+
+            <Card class={`${styles.card}`} style={{borderBottomColor: "#DFA67B", borderBottomStyle: "solid" ,borderWidth:"30px"}}>
                 <h3 style={{margin: "0px"}}>December</h3>
-                <p style={{margin: "2px"}}>25 December: Christmas Day*</p> //26
+                <p style={{margin: "2px"}}>25 December: Christmas Day*</p>
             </Card>
         </div>
-        <h3>* Off in Lieu </h3>
+        <div class={`${styles.row}`}>
+            <p>*Off in Lieu: 2 May, 11 July, 26 December</p>
+        </div>
+
     </Card>
 </div>
 </>
